f22e29e1c2496396c922d9ba753c901a76346d57
gsojc234 <gsojc234@gmail.com>
2024-11-15 17:39:23 +0900

    Implement AssertEqualAction

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/workflow/action/AssertEqualAction.java
@@ -0,0 +1,43 @@
+/*
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.tlsattacker.core.workflow.action;
+
+import de.rub.nds.tlsattacker.core.exceptions.ActionExecutionException;
+import de.rub.nds.tlsattacker.core.state.State;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+@XmlRootElement(name = "AssertEqualAction")
+public class AssertEqualAction<T> extends TlsAction {
+
+    private final Supplier<T> value1;
+    private final Supplier<T> value2;
+    private boolean comparisonResult;
+
+    public AssertEqualAction(Supplier<T> value1, Supplier<T> value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    @Override
+    public void execute(State state) throws ActionExecutionException {
+        comparisonResult = Objects.equals(value1.get(), value2.get());
+    }
+
+    @Override
+    public void reset() {
+        setExecuted(false);
+    }
+
+    @Override
+    public boolean executedAsPlanned() {
+        return comparisonResult;
+    }
+}
